c9055ead1c5358a33e2d1d98b51417431da24840
Rogerio Pereira <rogerio@colmeiatecnologia.com.br>
2019-08-20 16:04:01 -0400

    Dir e Help

---
+++ Dir_e_Help.py
@@ -0,0 +1,19 @@
+"""
+Utilitários Python para te auxiliar na programação
+
+dir(tipo de dado/variável)
+    Apresenta todos atributos e funções/métodos disponíveis para determinado tipo de dado ou variável.
+
+help(variavel.propriedade)
+    Apresenta a documentação/como utilizar os atributos/propriedades e funções/métodos disponíveis para
+        determinado tipo de dado ou variável
+"""
+dir("Geek")
+
+help("Geek".lower)
+help("Geek".upper)
+help("Geek".title)
+
+dir(4)
+num = 4
+help(num.real)
