9de0bff0a4ee90af4f64e0695aceb5c55600eeda
Rogerio Pereira <rogerio@colmeiatecnologia.com.br>
2019-08-22 13:56:22 -0400

    Tipo booleano

---
+++ TipoBooleano.py
@@ -0,0 +1,23 @@
+"""
+Tipo Booleano
+
+True e False
+Sempre com a inicial maiuscula
+
+Errado
+true, false
+Certo
+True, False
+
+negação (not)
+"""
+
+ativo = False
+print(ativo)
+print(not ativo)
+
+# or
+print(True or False)
+
+# and
+print(True and False)
