a0ebc4a90c98c38165c577965a6dbe12f7b21993
Rogerio Pereira <rogerio@colmeiatecnologia.com.br>
2019-08-22 13:25:53 -0400

    Tipo Float

---
+++ TipoFloat.py
@@ -0,0 +1,10 @@
+"""
+Tipo float
+"""
+
+# Python permite trabalhar com numeros complexos
+# Numero complexo elexado ao quadrado é negativo
+complexo = 5j
+print(type(complexo))
+complexo = complexo ** 2
+print(complexo) #-25
